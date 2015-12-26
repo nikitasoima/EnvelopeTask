5e4261ef5c7c1f3d525a8497dd3a5df69ac9a4e4
Alexey Sergeev <alexsergeev@i.ua>
2015-12-26 18:18:14 +0200

    Добавен ввод сторон

--- EnvelopeTask/Program.cs
+++ EnvelopeTask/Program.cs
@@ -10,10 +10,63 @@ namespace EnvelopeTask
     {
         static void Main(string[] args)
         {
+           double a,b,c,d;
+          
+
+             Console.WriteLine("Введите сторону a первого конверта: ");
+              try {
+
+                a = Convert.ToDouble(Console.ReadLine());
+                   }
+                   catch 
+                  {
+                      Console.WriteLine("Повторите попытку");
+                a = Convert.ToDouble(Console.ReadLine());
+                   }
+                Console.WriteLine("Введите сторону b первого конверта: ");
+            try
+            {
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Повторите попытку");
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("Введите сторону c второго конверта: ");
+            try
+            {
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Повторите попытку");
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите сторону d второго конверта: ");
+            try
+            {
+                d = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Повторите попытку");
+                d = Convert.ToDouble(Console.ReadLine());
+            }
 
             
-
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону c второго конверта: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону d второго конверта: ");
+            d = Convert.ToDouble(Console.ReadLine());
 
         }
+
+
+
+
+
     }
 }
